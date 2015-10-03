99929b920615651eccb79543ab62b1902b67ca7e
daniel-valcarce <valcarcel-01@hotmail.com>
2015-10-03 12:49:44 -0500

    segundo commit

---
+++ platforms/android/assets/www/central/central.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="msapplication-tap-highlight" content="no" />
+
+        <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, eight=device-height, target-densitydpi=device-dpi" />
+            <link rel="stylesheet" type="text/css" href="css/centralestilos.css" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+        <title>Central Servimoto</title>
+    </head>
+    
+    <body>
+       <div id="container">
+            <header>
+               <h1 id="titulo">Serim<img src="img/llanta.jpg">t<img src="img/llanta.jpg"></h1>
+            </header>
+            
+            <section>
+                
+            </section>
+            
+            <footer>
+                
+            </footer>
+        </div>
+    </body>
+  
+</html>
